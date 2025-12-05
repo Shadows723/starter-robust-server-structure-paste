9003ed675b5fb73b84d7e3aafcb3c7eced955eac
Michael <mspears171997@gmail.com>
2025-12-05 15:29:59 -0600

    Updating progress

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const pastesRouter = require("./pastes/pastes.router");
 const app = express();
 
+
+app.use(express.json())
 // TODO: Follow instructions in the checkpoint to implement ths API.
+const pastes = require("./data/paste-data");
+
+app.use("/pastes", pastesRouter);
 
 // Not found handler
 app.use((request, response, next) => {
@@ -11,7 +17,8 @@ app.use((request, response, next) => {
 // Error handler
 app.use((error, request, response, next) => {
   console.error(error);
-  response.send(error);
+  const {status = 500, message = "Something went wrong!" } = error;
+  response.status(status).json({ error: message });
 });
 
 module.exports = app;
